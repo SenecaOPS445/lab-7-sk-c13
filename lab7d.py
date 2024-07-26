ea4f789cd66e35c953ea2ae640945fc4af40c076
sk-c13 <sk-c13@myseneca.ca>
2024-07-26 13:52:49 -0400

    Update lab7d.py

--- lab7d.py
+++ lab7d.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id] 
+# Student ID: sk-c13
 class Time:
     """Simple object type for time of the day.
-        data attributes: hour, minute, second
-        function attributes: __init__, __str__, __repr__
+       data attributes: hour, minute, second
+       function attributes: __init__, __str__, __repr__
                             time_to_sec, format_time,
                             change_time, sum_time
     """
-    def __init__(self,hour=12,minute=0,second=0):
+    def __init__(self, hour=12, minute=0, second=0):
         """constructor for time object""" 
         self.hour = hour
         self.minute = minute
         self.second = second
-
+    
     def format_time(self):
         """Return time object (t) as a formatted string"""
         return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
-
+ 
     def sum_times(self, t2):
         """Add two time objests and return the sum."""
         self_sec = self.time_to_sec()
         t2_sec = t2.time_to_sec()
         sum = sec_to_time(self_sec + t2_sec)
         return sum
-
+    
     def change_time(self, seconds):
         time_seconds = self.time_to_sec()
         nt = sec_to_time(time_seconds + seconds)
@@ -41,14 +41,14 @@ class Time:
         """check for the validity of the time object attributes:
         24 > hour > 0, 60 > minute > 0, 60 > second > 0 """
         if self.hour < 0 or self.minute < 0 or self.second < 0:
-            return False
+           return False
         if self.minute >= 60 or self.second >= 60 or self.hour >= 24:
-            return False
+           return False
         return True
 
 def sec_to_time(seconds):
     '''convert a given number of seconds to a time object in 
-        hour, minute, second format'''
+       hour, minute, second format'''
     time = Time()
     minutes, time.second = divmod(seconds, 60)
     time.hour, time.minute = divmod(minutes, 60)
